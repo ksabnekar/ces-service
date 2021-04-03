ce0bcb1f22b57c828e0ecaa65575dee6614e0e7e
Santhosh <santhosh.veeramalla@mutualofomaha.com>
2021-04-02 23:38:52 -0500

    Changing dblite to postgres for heroku deployment

--- ces/settings.py
+++ ces/settings.py
@@ -82,25 +82,17 @@ WSGI_APPLICATION = 'ces.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         # 'ENGINE': 'django.db.backends.sqlite3',
-#         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'efs',
-#         'USER': 'postgres',
-#         'PASSWORD': 'admin',
-#         'PORT': '5433'
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'efs',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'PORT': '5433'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
